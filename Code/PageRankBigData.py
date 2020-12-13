928fc93ce4aa041d0eb2843cbd291d5a288336c7
Wei Wei <Wei.Wei@student.uantwerpen.be>
2020-12-13 10:24:34 +0100

    big data pagerank

---
+++ Code/PageRankBigData.py
@@ -0,0 +1,111 @@
+import sys
+from datetime import datetime
+
+from pyspark import SparkContext, SparkConf, StorageLevel
+from Config import teleportationProbability
+
+N = 50000000
+
+def updateIteration(entry):
+    # entry = ("0",[lijst outgoing])
+    n = len(entry[1])
+    p = int(entry[0][0])
+    PR = float(entry[0][1])
+    outlinks = entry[1]
+
+    value1 = []
+    for outlink in outlinks:
+        value1.append((outlink, PR / n))
+
+    value1.append((p, outlinks))
+    return value1
+
+
+def updateReduce(next):
+    # entry = ("0",[lijst outgoing])
+    # PR(di) := /N + (1-)j=1..m PRj(di);
+    # emit(key3: [di, PR(di)], value3: outlinks(di));
+    PRlist = next[1]
+    p = next[0]
+    Outlinks = []
+
+    PRSum = 0
+    for i in PRlist:
+        if (not isinstance(i, list)):
+            PRSum += i
+        else:
+            Outlinks = i
+
+    PR = teleportationProbability * 1 / N + (1 - teleportationProbability) * PRSum
+    return ((p, PR), Outlinks)
+
+
+def pageRank(data):
+    data = data.map(lambda x: ((x[0], 1 / N), x[1]))
+
+    rold = None
+    rnew = data
+    iter = 0
+    diff = sys.float_info.max
+    eps = 0.000001
+    while diff>eps:
+        timer = datetime.now()
+        iter += 1
+        rold = rnew
+        rnew = rold.flatMap(updateIteration).groupByKey().map(updateReduce)
+
+        # calculate difference?
+        if(iter>=0):
+            #persist both of the rdd to make sure a shuffle join happends, which doesn't cause timeout.
+            mapped1=rnew.map(lambda x:x[0]).persist(storageLevel=StorageLevel.MEMORY_AND_DISK)
+            mapped2=rold.map(lambda x:x[0]).persist(storageLevel=StorageLevel.MEMORY_AND_DISK)
+            diff=mapped1.join(mapped2).map(lambda x:abs(x[1][0]-x[1][1]))
+            diff=diff.sum()
+            print("Iteration ", iter, " just finished with first order norm: ", diff, " approximated elapsed time = ",
+                 (datetime.now() - timer).total_seconds())
+            #unpersist to save space.
+            mapped1.unpersist()
+            mapped2.unpersist()
+        else:
+            print("Iteration ", iter, " just finished, approximated elapsed time = ",
+                 (datetime.now() - timer).total_seconds())
+
+    # sort by descending order of page ranking values
+    result = rnew.map(lambda x: x[0]).sortBy(lambda x: x[1], False)
+    # print(result)
+    return result
+
+
+if __name__ == '__main__':
+    conf = SparkConf()
+    conf.set("spark.network.timeout", "36001s")
+    conf.set("spark.executor.heartbeatInterval","36000s")
+    conf.set("spark.storage.blockManagerSlaveTimeoutMs","36000s")
+    conf.set("spark.worker.timeout","36000s")
+    conf.set("spark.sql.broadcastTimeout","36000s")
+    conf.set("spark.executor.memory", "5g")
+    conf.set("spark.driver.memory", "12g")
+    conf.set("spark.executor.cores", "5")
+    conf.set("spark.worker.cleanup.enabled", "true")
+    sc = SparkContext("local[5]", "PageRanking",conf=conf)
+
+
+    # filtering mini database to be of form (from node, list of to nodes)
+    data = sc.textFile("./Dataset/ClueWeb09_WG_50m.graph-txt").zipWithIndex()
+
+    #save the values as int to save space
+    data = data.filter(lambda x: int(x[1])!=0 and int(x[1])<=50000000).map(lambda x: (
+        int(x[1])-1, list(map(lambda x: int(x), filter(lambda x: x != "" and int(x) <= 50000000, str(x[0]).split(" "))))))
+
+    # # (0,[]) <-
+    # # (score) <- rold
+
+
+
+    result = pageRank(data)
+
+    # write result to csv file (fails because OOM when collecting result.)
+    result = result.map(lambda line: str(line)[1:-1])
+    rankFile = open('ranking_clueweb_0.15.csv', 'w')
+    rankFile.write("nodeID,pageRankScore\n")
+    rankFile.write("\n".join(result.collect()))
