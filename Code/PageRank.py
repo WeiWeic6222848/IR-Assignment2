c1d5a121158304e650b5cf00aa3669e7f39bff56
wei Wei <Wei.Wei@student.uantwerpen.be>
2020-12-14 15:41:37 +0100

    simplified preprocessing and fixed problem with deadend implementation

--- Code/PageRank.py
+++ Code/PageRank.py
@@ -5,12 +5,13 @@ from pyspark import SparkContext, SparkConf, StorageLevel
 from Config import teleportationProbability
 
 N = 875713
-deadendpool = 0.0
+deadendpool = 0
 
 def updateIteration(entry):
     # entry = ("0",[lijst outgoing])
+    p = entry[0][0]
+
     n = len(entry[1])
-    p = int(entry[0][0])
     PR = float(entry[0][1])
     outlinks = entry[1]
 
@@ -18,10 +19,8 @@ def updateIteration(entry):
     for outlink in outlinks:
         value1.append((outlink, PR / n))
 
-    # if outlink is empty, add PR to the deadend pool
-    if (len(outlinks) == 0):
-        global deadendpool
-        deadendpool += PR
+    if (len(outlinks)==0):
+        deadendaccumulator.add(PR)
 
     value1.append((p, outlinks))
     return value1
@@ -31,8 +30,9 @@ def updateReduce(next):
     # entry = ("0",[lijst outgoing])
     # PR(di) := /N + (1-)j=1..m PRj(di);
     # emit(key3: [di, PR(di)], value3: outlinks(di));
-    PRlist = next[1]
     p = next[0]
+
+    PRlist = next[1]
     Outlinks = []
 
     PRSum = 0
@@ -42,7 +42,7 @@ def updateReduce(next):
         else:
             Outlinks = i
 
-    PR = teleportationProbability * 1 / N + (1 - teleportationProbability) * PRSum + (1-teleportationProbability) * deadendpool / N
+    PR = teleportationProbability * 1 / N + (1 - teleportationProbability) * PRSum + (1-teleportationProbability) * deadendpool.value / N
     return ((p, PR), Outlinks)
 
 
@@ -58,13 +58,24 @@ def pageRank(data):
         timer = datetime.now()
         iter += 1
         rold = rnew
-        rnew = rold.flatMap(updateIteration).groupByKey().map(updateReduce)
+        rnew = rold.flatMap(updateIteration)
+
+        global deadendpool
+        val = deadendaccumulator.value
+        deadendpool = sc.broadcast(val)
+        deadendaccumulator.add(-val) #reset deadendpool
+
+
+        rnew = rnew.groupByKey().map(updateReduce)
+        #print(deadendpool)
 
         # calculate difference?
         if(iter>=0):
             mapped1=rnew.map(lambda x:x[0]).persist(storageLevel=StorageLevel.MEMORY_AND_DISK)
             mapped2=rold.map(lambda x:x[0]).persist(storageLevel=StorageLevel.MEMORY_AND_DISK)
-            diff=mapped1.join(mapped2).map(lambda x:abs(x[1][0]-x[1][1]))
+            #print(mapped2.map(lambda x:x[1]).sum())
+            diff=mapped1.join(mapped2)
+            diff=diff.map(lambda x:abs(x[1][0]-x[1][1]))
             diff=diff.max()
             print("Iteration ", iter, " just finished with first order norm: ", diff, " approximated elapsed time = ",
                  (datetime.now() - timer).total_seconds())
@@ -79,6 +90,11 @@ def pageRank(data):
     return result
 
 
+def addEntries(entry):
+    entry=entry.split("\t")
+    yield (int(entry[0].strip()),[int(entry[1].strip())])
+    yield (int(entry[1].strip()),[])
+
 if __name__ == '__main__':
     conf = SparkConf()
     conf.set("spark.network.timeout", "36001s")
@@ -89,13 +105,17 @@ if __name__ == '__main__':
     conf.set("spark.executor.memory", "5g")
     conf.set("spark.driver.memory", "8g")
     conf.set("spark.worker.cleanup.enabled", "true")
-    sc = SparkContext("local[*]", "PageRanking",conf=conf)
+    sc = SparkContext("local[3]", "PageRanking",conf=conf)
+    deadendaccumulator = sc.accumulator(0)
 
 
     # filtering mini database to be of form (from node, list of to nodes)
     data = sc.textFile("./Dataset/web-Google.txt").filter(lambda l: not str(l).startswith("#")) \
-        .map(lambda x: x.split("\t")).map(lambda x: (int(x[0].strip()), int(x[1].strip()))).groupBy(
-        lambda x: x[0]).mapValues(lambda x: list(map(lambda y: int(y[1]), x)))
+        .flatMap(addEntries)\
+        .reduceByKey(lambda x,y:x+y)
+    # data = sc.parallelize([(0, [2]), (1, [2]), (2, [0, 1, 3]), (3, [])])
+
+    #    t=data.collect()
 
     result = pageRank(data)
 
